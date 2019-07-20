bdf8632747e61bbc76faf0541333735f976ecdeb
nignanthomas <nignanthomas@gmail.com>
2019-07-20 14:49:52 +0300

    [chore #167367580] define signin test

---
+++ test/signin.test.js
@@ -0,0 +1,25 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../api/server';
+
+// eslint-disable-next-line no-unused-vars
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('Sgin In', () => {
+  it('POST /api/v1/auth/signin Should log in a user', (done) => {
+    const user = {
+      email: 'nignanthomas@gmail.com',
+      password: 'qwerty',
+    };
+    chai
+      .request(app)
+      .post('/api/v1/auth/signin')
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(201);
+        res.body.should.be.a('object');
+        done();
+      });
+  });
+});
