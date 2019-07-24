a3abe56ec4d66f72678e0c63d487aaceb8690404
nignanthomas <nignanthomas@gmail.com>
2019-07-24 10:12:31 +0300

    [Delivers #167455242] formatBooking now in controllers/bookings works fine => send the required output

--- api/v1/controllers/bookings.js
+++ api/v1/controllers/bookings.js
@@ -1,4 +1,24 @@
 import BookingModel from '../models/bookingModel';
+import UserModel from '../models/userModel';
+import TripModel from '../models/tripModel';
+
+/**
+*
+* @param {object} data
+* @returns {object} formatted booking object
+*/
+const formatBooking = (data) => {
+  const formatted = {
+    id: data.id,
+    bus_license_number: TripModel.getOneTrip(data.trip_id).bus_license_number,
+    trip_date: UserModel.getOneUser(data.user_id).trip_date,
+    first_name: UserModel.getOneUser(data.user_id).first_name,
+    last_name: UserModel.getOneUser(data.user_id).last_name,
+    user_email: UserModel.getOneUser(data.user_id).email,
+    seat_number: data.seat_number,
+  };
+  return formatted;
+};
 
 const Booking = {
   /**
@@ -8,7 +28,7 @@ const Booking = {
   */
   createBooking(req, res) {
     const { body } = req;
-    if (!body.user_id || !body.trip_id) {
+    if (!body.user_id || !body.trip_id || !body.seat_number) {
       return res.status(400).json({ status: 'error', error: 'Bad Request! All booking fields are required!' });
     }
     const booking = BookingModel.book(body);
@@ -25,7 +45,12 @@ const Booking = {
     if (!bookings.length) {
       return res.status(404).json({ status: 'Oops! No bookings found!', data: [] });
     }
-    return res.status(200).json({ status: 'success', data: bookings });
+    const formattedBookings = [];
+    bookings.forEach((booking) => {
+      const formattedBooking = formatBooking(booking);
+      formattedBookings.push(formattedBooking);
+    });
+    return res.status(200).json({ status: 'success', data: formattedBookings });
   },
 
   /**
@@ -37,7 +62,8 @@ const Booking = {
     const bookingId = parseInt(req.params.bookingId, 10);
     const oneBooking = BookingModel.getOneBooking(bookingId);
     if (oneBooking) {
-      return res.status(200).json({ status: 'success', data: oneBooking });
+      const formattedBooking = formatBooking(oneBooking);
+      return res.status(200).json({ status: 'success', data: formattedBooking });
     }
     return res.status(404).json({ status: 'error', error: `Cannot find booking of id: ${bookingId}` });
   },
@@ -52,7 +78,8 @@ const Booking = {
     const oneBooking = BookingModel.getOneBooking(bookingId);
     if (oneBooking) {
       const updatedBooking = BookingModel.updateBooking(bookingId, req.body);
-      return res.status(200).json({ status: 'success', data: { message: 'Booking Updated Successfully!', data: updatedBooking } });
+      const formattedBooking = formatBooking(updatedBooking);
+      return res.status(200).json({ status: 'success', data: { message: 'Booking Updated Successfully!', data: formattedBooking } });
     }
     return res.status(404).json({ status: 'error', error: `Cannot find booking of id: ${bookingId}` });
   },
