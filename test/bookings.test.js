4b86c1c9aab466f3dbd6396da56b19ae0f33fd70
nignanthomas <nignanthomas@gmail.com>
2019-07-30 07:27:21 +0200

    [Chore] write more tests for bookings

--- test/bookings.test.js
+++ test/bookings.test.js
@@ -1,6 +1,5 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import moment from 'moment';
 import app from '../api/server';
 import BookingModel from '../api/v1/models/bookingModel';
 
@@ -9,97 +8,178 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('Bookings Tests', () => {
-  it('POST /api/v1/bookings Should create a new booking object', (done) => {
-    const booking = {
-      id: BookingModel.getAllBookings().length + 1,
-      trip_id: 1,
-      user_id: 2,
-      seat_number: 12,
-      create_on: moment().format('dddd, MMMM Do YYYY, h:mm:ss a'),
-    };
-    chai
-      .request(app)
-      .post('/api/v1/bookings')
-      .send(booking)
-      .end((err, res) => {
-        res.should.have.status(201);
-        res.body.should.be.a('object');
-        done();
-      });
-  });
-
-  it('GET /api/v1/bookings Should return all bookings', (done) => {
-    chai
-      .request(app)
-      .get('/api/v1/bookings')
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        done();
-      });
-  });
-
-  it('GET /api/v1/bookings/:id Should return a specific booking', (done) => {
-    const booking = {
-      id: BookingModel.getAllBookings().length + 1,
-      trip_id: 1,
-      user_id: 2,
-      seat_number: 12,
-      create_on: moment().format('dddd, MMMM Do YYYY, h:mm:ss a'),
-    };
-    const bookingId = BookingModel.book(booking).id;
-    chai
-      .request(app)
-      .get(`/api/v1/bookings/${bookingId}`)
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        done();
-      });
-  });
-
-  it('PATCH /api/v1/bookings/:id Should update a given booking', (done) => {
-    const booking = {
-      id: BookingModel.getAllBookings().length + 1,
-      trip_id: 1,
-      user_id: 2,
-      seat_number: 12,
-      create_on: moment().format('dddd, MMMM Do YYYY, h:mm:ss a'),
-    };
-    const bookingId = BookingModel.book(booking).id;
-    chai
-      .request(app)
-      .patch(`/api/v1/bookings/${bookingId}`)
-      .send({
-        id: BookingModel.getAllBookings().length + 1,
-        trip_id: 2,
+  describe('POST bookings tests', () => {
+    it('POST /api/v1/bookings Should create a new booking object', (done) => {
+      const booking = {
+        trip_id: 1,
         user_id: 2,
         seat_number: 12,
-        create_on: moment().format('dddd, MMMM Do YYYY, h:mm:ss a'),
-      })
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        done();
-      });
+      };
+      chai
+        .request(app)
+        .post('/api/v1/bookings')
+        .send(booking)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          res.body.status.should.match(/success/);
+          res.body.data.seat_number.should.equal(12);
+          done();
+        });
+    });
+
+    it('POST /api/v1/bookings Should not create a new booking object (No trip_id)', (done) => {
+      const booking = {
+        user_id: 2,
+        seat_number: 12,
+      };
+      chai
+        .request(app)
+        .post('/api/v1/bookings')
+        .send(booking)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.a('object');
+          res.body.status.should.match(/error/);
+          done();
+        });
+    });
+
+    it('POST /api/v1/bookings Should not create a new booking object (No user_id)', (done) => {
+      const booking = {
+        trip_id: 2,
+        seat_number: 12,
+      };
+      chai
+        .request(app)
+        .post('/api/v1/bookings')
+        .send(booking)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.a('object');
+          res.body.status.should.match(/error/);
+          done();
+        });
+    });
+
+    it('POST /api/v1/bookings Should not create a new booking object (No seat_number)', (done) => {
+      const booking = {
+        trip_id: 2,
+        user_id: 1,
+      };
+      chai
+        .request(app)
+        .post('/api/v1/bookings')
+        .send(booking)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.a('object');
+          res.body.status.should.match(/error/);
+          done();
+        });
+    });
   });
 
-  it('DELETE /api/v1/bookings/:id Should delete a given booking', (done) => {
-    const booking = {
-      id: BookingModel.getAllBookings().length + 1,
-      trip_id: 1,
-      user_id: 2,
-      seat_number: 12,
-      create_on: moment().format('dddd, MMMM Do YYYY, h:mm:ss a'),
-    };
-    const bookingId = BookingModel.book(booking).id;
-    chai
-      .request(app)
-      .delete(`/api/v1/bookings/${bookingId}`)
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        done();
-      });
+  describe('GET all bookings tests', () => {
+    it('GET /api/v1/bookings Should return all bookings', (done) => {
+      chai
+        .request(app)
+        .get('/api/v1/bookings')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.status.should.match(/success/);
+          res.body.data.should.be.a('array');
+          done();
+        });
+    });
+  });
+
+  describe('GET a booking tests', () => {
+    it('GET /api/v1/bookings/:id Should return a specific booking', (done) => {
+      const booking = {
+        trip_id: 1,
+        user_id: 2,
+        seat_number: '4A',
+      };
+      const bookingId = BookingModel.book(booking).id;
+      chai
+        .request(app)
+        .get(`/api/v1/bookings/${bookingId}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.status.should.match(/success/);
+          res.body.data.seat_number.should.match(/4A/);
+          done();
+        });
+    });
+
+    it('GET /api/v1/bookings/:id Should not get a specific booking (Booking 11 that does not exist)', (done) => {
+      chai
+        .request(app)
+        .get('/api/v1/bookings/11')
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a('object');
+          res.body.status.should.match(/error/);
+          done();
+        });
+    });
+
+    it('GET /api/v1/bookings/:id Should not get a specific booking (Booking \'a\' is not a valid user id)', (done) => {
+      chai
+        .request(app)
+        .get('/api/v1/bookings/a')
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a('object');
+          res.body.status.should.match(/error/);
+          done();
+        });
+    });
+  });
+
+  describe('PATCH a booking tests', () => {
+    it('PATCH /api/v1/bookings/:id Should update a given booking', (done) => {
+      const booking = {
+        trip_id: 1,
+        user_id: 2,
+        seat_number: 12,
+      };
+      const bookingId = BookingModel.book(booking).id;
+      chai
+        .request(app)
+        .patch(`/api/v1/bookings/${bookingId}`)
+        .send({
+          trip_id: 2,
+          user_id: 2,
+          seat_number: 12,
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+  });
+
+  describe('DELETE bookings tests', () => {
+    it('DELETE /api/v1/bookings/:id Should delete a given booking', (done) => {
+      const booking = {
+        trip_id: 1,
+        user_id: 2,
+        seat_number: 12,
+      };
+      const bookingId = BookingModel.book(booking).id;
+      chai
+        .request(app)
+        .delete(`/api/v1/bookings/${bookingId}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        });
+    });
   });
 });
